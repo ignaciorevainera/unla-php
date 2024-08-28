319494abdf35e3ff9d33dbf2d712c28b1285a11a
Ignacio Revainera <ignacio.revainera@hotmail.com>
2024-08-28 15:58:20 -0300

    feat: add favicon

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -4,8 +4,8 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="icon" type="image/svg+xml" href="/assets/images/favicon.svg">
-	<link rel="icon" type="image/png" href="/assets/images/favicon.png">
+	<link rel="icon" type="image/svg+xml" href=<?php echo base_url('/assets/favicon/favicon.svg'); ?>>
+	<link rel="icon" type="image/png" href=<?php echo base_url('/assets/favicon/favicon.png'); ?>>
 	<title>
 		<?php echo isset($title) ? $title : 'Mi Sitio Web'; ?>
 	</title>
