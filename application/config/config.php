4bb5965dbfd78c1620585b1497931ae4899c80eb
Ignacio Revainera <ignacio.revainera@hotmail.com>
2024-08-30 19:37:22 -0300

    build: remove database library

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/codeigniter-develop/';
+$config['base_url'] = 'http://localhost/unla-php/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/codeigniter-develop/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
